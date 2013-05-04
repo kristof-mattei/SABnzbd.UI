f4b5ed0c2f93e989ae3c283d32e312e8da749ed9
Kristof Mattei <github@kristofmattei.be>
2013-05-04 13:47:09 +0200

    Added Management DLL + Management DLL Tests + Test stub

---
+++ Management.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Management.Tests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			Class1 c = new Class1();
+
+			Assert.AreEqual(c, c);
+		}
+	}
+}
