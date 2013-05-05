3afa2b35f30a5913e09fc00a96c525848097bcb0
Kristof Mattei <github@kristofmattei.be>
2013-05-05 22:05:33 +0200

    Added fields for the Api + fields to talk to.

--- Model/Queue.cs
+++ Model/Queue.cs
@@ -1,19 +1,40 @@
 ﻿namespace Model
 {
-	using System.Collections.ObjectModel;
+	using System.Diagnostics;
 	using System.Xml.Serialization;
 
 	[XmlRoot("queue")]
 	public class Queue
 	{
-		/// <summary>
-		///     Implemented as True or False on the Api, so that's easily convertible
-		/// </summary>
+		#region "These are for the serializer, don't use them directly, they're not useful"
+
 		[XmlElement("paused")]
-		public bool Paused { get; set; }
+		[DebuggerBrowsable(DebuggerBrowsableState.Never)]
+		[UsedImplicitly]
+		public string ApiPaused;
+
+		#endregion
+
+		/// <summary>
+		/// TODO check type on this one and whether we need a custom backing field
+		/// Represents the current speed
+		/// </summary>
+		[XmlElement("speed")]
+		public decimal Speed { get; set; }
+
+		/// <summary>
+		///     Paused or not, uses the <see cref="ApiPaused" />
+		/// </summary>
+		public bool Paused
+		{
+			get
+			{
+				return this.ApiPaused == "True";
+			}
+		}
 
 		[XmlArray("slots")]
-		[XmlArrayItem(typeof(Slot), ElementName = "slot")]
+		[XmlArrayItem(typeof (Slot), ElementName = "slot")]
 		public Slot[] Slots { get; set; }
 	}
 }
